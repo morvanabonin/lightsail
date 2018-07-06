da45fc89603834e425150f0511b9e3fd9429e617
guilherme.fontans <guilherme.fontans@cyberweb.com.br>
2018-07-06 19:11:00 -0300

    Adição de mais alguns metodos da api do lightSail

--- bin.php
+++ bin.php
@@ -7,4 +7,21 @@ require 'vendor/autoload.php';
 use LightSail\LightSailProvider;
 
 $lightsail = new LightSailProvider();
-echo $lightsail->getInstances();
+//echo $lightsail->getInstances();
+
+//print_r($lightsail->getBundles());
+
+//print_r($lightsail->getBlueprints());
+
+//print_r($lightsail->startInstance("deathnote"));
+//print_r($lightsail->getRegions());
+
+/*$lightsail->createInstance([
+    'availabilityZone' => 'us-east-2a',
+    'blueprintId' => 'ubuntu_16_04_2',
+    'bundleId' => 'nano_1_0',
+    'customImageName' => null,
+    'instanceNames' => ["deathNote2"],
+    'customImageName' => null,
+    'userData' => 'apt-get upgrade'
+]);*/
--- src/LightSailProvider.php
+++ src/LightSailProvider.php
@@ -23,7 +23,11 @@ class LightSailProvider
     public function __construct()
     {
         $this->credentials = new Credentials(self::KEY, self::SECRET);
-        $this->lightSailClient = new LightsailClient([ "region" => 'us-east-2', "version" => "latest", 'credentials' => $this->credentials]);
+        $this->lightSailClient = new LightsailClient([
+            "region" => 'us-east-2',
+            "version" => "latest",
+            "credentials" => $this->credentials
+        ]);
     }
 
     /**
@@ -33,5 +37,46 @@ class LightSailProvider
         return $this->lightSailClient->getInstances();
     }
 
+    public function startInstance($instance)
+    {
+        return $this->lightSailClient->startInstance(["instanceName" => $instance]);
+    }
 
+    public function stopInstance($instance, $force = false)
+    {
+        return $this->lightSailClient->stopInstance([
+            "force" => $force,
+            "instanceName" => $instance
+        ]);
+    }
+
+    public function getBlueprints()
+    {
+        return $this->lightSailClient->getBlueprints();
+    }
+
+    public function getBundles()
+    {
+        return $this->lightSailClient->getBundles();
+    }
+
+    public function getRegions()
+    {
+        return $this->lightSailClient->getRegions([
+            'includeAvailabilityZones' => true
+        ]);
+    }
+
+    public function createInstance($params)
+    {
+        return $this->lightSailClient->createInstances([
+            'availabilityZone' => $params['availabilityZone'],
+            'blueprintId' => $params['blueprintId'],
+            'bundleId' => $params['bundleId'],
+            'customImageName' => $params[''],
+            'instanceNames' => $params['instanceNames'],
+            'keyPairName' => $params['keyPairName'],
+            'userData' => $params['userData']
+        ]);
+    }
 }
