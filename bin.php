908008de1bfc259c028f15a49029727378daeee0
guilherme.fontans <guilherme.fontans@cyberweb.com.br>
2018-07-09 10:49:09 -0300

    Adição de alguns metodos da api

--- bin.php
+++ bin.php
@@ -7,7 +7,24 @@ require 'vendor/autoload.php';
 use LightSail\LightSailProvider;
 
 $lightsail = new LightSailProvider();
-print_r($lightsail->stopInstance());
+//print_r($lightsail->getInstances());
+print_r($lightsail->startInstance());
 /*$lightsail->getDataInstace();
 $lightsail->getInstance();*/
 //echo $lightsail->startInstance();
+//print_r($lightsail->getBundles());
+
+//print_r($lightsail->getBlueprints());
+
+//print_r($lightsail->getRegions());
+
+/*$lightsail->createInstance([
+    'availabilityZone' => 'us-east-2a',
+    'blueprintId' => 'ubuntu_16_04_2',
+    'bundleId' => 'nano_1_0',
+    'customImageName' => null,
+    'instanceNames' => ["deathNote2"],
+    'customImageName' => null,
+    'userData' => 'apt-get upgrade'
+]);*/
+
